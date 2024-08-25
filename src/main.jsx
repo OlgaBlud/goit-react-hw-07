ae0a2fb264a5eb21c91e081f9240ec28f7cd8783
Olga Blud <olga.blud@gmail.com>
2024-08-25 18:50:46 +0200

    removee persistor local storage

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { persistor, store } from "./redux/store.js";
-import { PersistGate } from "redux-persist/integration/react";
+import { store } from "./redux/store.js";
+// import { persistor, store } from "./redux/store.js";
+// import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      {/* <PersistGate persistor={persistor}> */}
+      <App />
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,35 +1,41 @@
 import { configureStore } from "@reduxjs/toolkit";
-
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
 import { filtersReducer } from "./filtersSlice";
 import { contactsReducer } from "./contactsSlice";
 
-const contactsConfig = {
-  key: "contacts",
-  storage,
-  whitelist: ["items"],
-};
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(contactsConfig, contactsReducer),
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-export const persistor = persistStore(store);
+
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+
+// const contactsConfig = {
+//   key: "contacts",
+//   storage,
+//   whitelist: ["items"],
+// };
+// export const store = configureStore({
+//   reducer: {
+//     contacts: persistReducer(contactsConfig, contactsReducer),
+//     filters: filtersReducer,
+//   },
+// middleware: (getDefaultMiddleware) =>
+//   getDefaultMiddleware({
+//     serializableCheck: {
+//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//     },
+//   }),
+// });
+// export const persistor = persistStore(store);
